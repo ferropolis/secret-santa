3f85a10ca396b0c4d192d86e49eab1cdb2bd3f29
ferropolis <77990395+ferropolis@users.noreply.github.com>
2025-09-15 22:54:07 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+date_default_timezone_set('Europe/Berlin');
+
+const DB_HOST = 'XXX';
+const DB_NAME = 'XXX';
+const DB_USER = 'XXX';
+const DB_PASS = 'XXX';
+
+const SITE_FROM_EMAIL = 'wichteln-noreply@tld.com';
+const SITE_FROM_NAME  = 'Secret Santa';
